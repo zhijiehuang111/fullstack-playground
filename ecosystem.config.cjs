0c6b9b7bc6ae07cd875dd81f17e08e3705a772db
zhijiehuang <zhijiehuang111@gmail.com>
2026-05-10 20:04:20 +0800

    feat: add interpreter configuration for API in ecosystem file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,6 +4,7 @@ module.exports = {
       name: "api",
       cwd: __dirname,
       script: "./apps/api/dist/index.js",
+      interpreter: process.execPath,
       node_args: "--env-file=.env",
       exec_mode: "fork",
       instances: 1,
